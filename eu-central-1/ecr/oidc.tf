1e0c52c9277ec6fd7350072d00a8eb54c0ecc411
MartinovG <gabismart7@yahoo.com>
2025-12-11 14:34:33 +0200

    Updating the OIDC github repo list

--- eu-central-1/ecr/oidc.tf
+++ eu-central-1/ecr/oidc.tf
@@ -2,7 +2,11 @@ module "github-oidc" {
     source = "unfunco/oidc-github/aws"
     version = "2.0.2"
 
-    github_repositories = ["MartinovG/DiplomaProject-Terraform:*"]
+    github_repositories = [
+        "MartinovG/DiplomaProject-Terraform:*",
+        "MartinovG/DiplomaProject-App:*",
+        "MartinovG/DiplomaProject-ArgoCD:*"
+        ]
 
     iam_role_inline_policies = {
         github_oidc_policy = data.aws_iam_policy_document.oidc_policy.json

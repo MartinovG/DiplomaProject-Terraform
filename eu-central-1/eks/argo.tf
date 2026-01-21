9550ce8efaa0144552d1ad88917e49c024d1b781
MartinovG <gabismart7@yahoo.com>
2026-01-21 11:22:34 +0200

    minor structure fix

--- eu-central-1/eks/argo.tf
+++ eu-central-1/eks/argo.tf
@@ -8,6 +8,7 @@ locals {
         "application.namespaces" = "*"
       }
     }
+    
     server = {
       ingress = {
         enabled          = true
@@ -18,31 +19,32 @@ locals {
           "alb.ingress.kubernetes.io/target-type"      = "ip"
           "alb.ingress.kubernetes.io/healthcheck-path" = "/"
           "alb.ingress.kubernetes.io/backend-protocol" = "HTTP"
-
-          "external-dns.alpha.kubernetes.io/hostname" = local.argocd_hostname
-
-          "alb.ingress.kubernetes.io/listen-ports" = jsonencode([{ "HTTP" = 80 }, { "HTTPS" = 443 }])
-          "alb.ingress.kubernetes.io/ssl-redirect" = "443"
-          "alb.ingress.kubernetes.io/certificate-arn" = data.aws_acm_certificate.argo.arn
+          "external-dns.alpha.kubernetes.io/hostname"  = local.argocd_hostname
+          "alb.ingress.kubernetes.io/listen-ports"     = jsonencode([{ "HTTP" = 80 }, { "HTTPS" = 443 }])
+          "alb.ingress.kubernetes.io/ssl-redirect"     = "443"
+          "alb.ingress.kubernetes.io/certificate-arn"  = data.aws_acm_certificate.argo.arn
         }
         tls = [{
           hosts = [local.argocd_hostname]
         }]
       }
-      
       resources = {
-        server = {
-          requests = { cpu = "50m", memory = "64Mi" }
-          limits   = { cpu = "200m", memory = "256Mi" }
-        }
-        controller = {
-          requests = { cpu = "50m", memory = "64Mi" }
-          limits   = { cpu = "500m", memory = "512Mi" }
-        }
-        redis = {
-          requests = { cpu = "50m", memory = "64Mi" }
-          limits   = { cpu = "100m", memory = "128Mi" }
-        }
+        requests = { cpu = "50m", memory = "64Mi" }
+        limits   = { cpu = "200m", memory = "256Mi" }
+      }
+    }
+    
+    controller = {
+      resources = {
+        requests = { cpu = "50m", memory = "64Mi" }
+        limits   = { cpu = "500m", memory = "512Mi" }
+      }
+    }
+    
+    redis = {
+      resources = {
+        requests = { cpu = "50m", memory = "64Mi" }
+        limits   = { cpu = "100m", memory = "128Mi" }
       }
     }
   }
@@ -52,13 +54,15 @@ resource "helm_release" "argocd" {
   name             = "argo-cd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
+  version          = "9.3.4" 
   namespace        = "argocd"
   create_namespace = true
   cleanup_on_fail  = true
 
-  depends_on = [module.eks]
-
-  values = [
-    yamlencode(local.argocd_values)
+  depends_on = [
+    module.eks,
+    module.alb_controller 
   ]
+
+  values = [yamlencode(local.argocd_values)]
 }
--- eu-central-1/eks/controllers.tf
+++ eu-central-1/eks/controllers.tf
@@ -22,6 +22,8 @@ module "dns_controller" {
     deployment_service_account = "external-dns"
     cluster_identity_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
     cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+
+    depends_on = [module.eks]
 }
 
 module "alb_controller" {
@@ -42,6 +44,8 @@ module "alb_controller" {
         vpcId = data.aws_vpc.current.id
         region = var.aws_region
     }
+
+    depends_on = [module.eks]
 }
 
 module "ack_rds_controller" {
@@ -49,9 +53,12 @@ module "ack_rds_controller" {
   cluster_name = module.eks.cluster_name
   aws_region = var.aws_region
   helm_chart_name = "rds-chart"
+  helm_chart_version = "1.7.3"
   helm_repo_url = "oci://public.ecr.aws/aws-controllers-k8s"
   helm_release_name = "ack-rds-controller"
 
   cluster_identity_oidc_issuer_url = module.eks.cluster_oidc_issuer_url
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+
+  depends_on = [module.eks]
 }
